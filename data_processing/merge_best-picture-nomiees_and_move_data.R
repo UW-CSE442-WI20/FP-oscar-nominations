1f8d003377b6268299db692674ce00fdb1c48dcd
Shih Hsuan (Daniel) Hsu <shhsu@cs.washington.edu>
2020-02-29 22:28:39 -0800

    merged best pic data

--- data_processing/merge_best-picture-nomiees_and_move_data.R
+++ data_processing/merge_best-picture-nomiees_and_move_data.R
@@ -1,4 +1,4 @@
-pacman::p_load(dplyr, pracma)
+pacman::p_load(dplyr, lubridate)
 
 # load data
 nomiees <- read.csv("best-picture-nomiees.csv", stringsAsFactors = FALSE)
@@ -14,7 +14,13 @@ names(nomiees)[5] <- "title"
 
 # merge them
 merged <- 
-  merge(nomiees, meta_trimmed, by = "title", all = T)
+  merge(nomiees, meta_trimmed, by = "title", all = T) %>%
+  filter(!is.na(is_winner)) %>%
+  transform(release_date = as.Date(release_date)) %>%
+  mutate(award_year = as.Date(ISOdate(year, 12, 31)))
 merged <-
-  merge(merged, meta_trimmed, by = "original_title", x.all = T) %>%
-  filter(!is.na(is_winner))
+  merged %>%
+  filter(award_year - release_date < years(2))
+
+# no data on 1974, 1975, 1990, 2014, and 2017
+write.csv(merged, "best-picture.csv")
