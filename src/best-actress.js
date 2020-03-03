e97a2516600555f1c8d08d07688fb058e3298fd4
Shih-Hsuan (Daniel) Hsu <shhsu@cs.washington.edu>
2020-03-02 16:23:44 -0800

    add more style

--- src/best-actress.js
+++ src/best-actress.js
@@ -61,7 +61,7 @@ d3.csv(csv)
         // Set the dimensions and margins of the diagram
         var margin = {top: 20, right: 90, bottom: 30, left: 200},
             width = 1300 - margin.left - margin.right,
-            height = 1000 - margin.top - margin.bottom;
+            height = 900 - margin.top - margin.bottom;
 
         // append the svg object to the body of the page
         // appends a 'group' element to 'svg'
