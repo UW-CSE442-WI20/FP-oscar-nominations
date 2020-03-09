7697e7b5193b37422d63666eee6d934a9a49a1f7
Shih-Hsuan (Daniel) Hsu <shhsu@cs.washington.edu>
2020-03-09 12:25:13 -0700

    add browser check

--- src/index.js
+++ src/index.js
@@ -1,23 +1,4 @@
-
-/*
-// You can require libraries
-const d3 = require('d3');
-const top10 = require('./top10-bar.js');
-// You can include local JS files:
-
-const myClassInstance = new MyClass();
-myClassInstance.sayHi();
-
-
-// You can load JSON files directly via require.
-// Note this does not add a network request, it adds
-// the data directly to your JavaScript bundle.
-const exampleData = require('./example-data.json');
-
-
-// Anything you put in the static folder will be available
-// over the network, e.g.
-d3.csv('carbon-emissions.csv')
-  .then((data) => {
-    console.log('Dynamically loaded CSV data', data);
-})*/
+// check if opened using Chrome
+if (!String(navigator.userAgent).includes("Chrome")) {
+  alert("Please view this page in Chrome!")
+}
